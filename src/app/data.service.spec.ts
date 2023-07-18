2c9e82802236ae6067e79f45af9a6b70674baf34
qrowned <admin@qrowned.dev>
2023-07-18 15:53:43 +0200

    Fix all existing tests

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DataService } from './data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('DataServiceService', () => {
   let service: DataService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(DataService);
   });
 
--- src/app/dialog.service.spec.ts
+++ src/app/dialog.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { DialogService } from './dialog.service';
+import { NxModalModule } from '@aposin/ng-aquila/modal';
 
 describe('DialogService', () => {
   let service: DialogService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [NxModalModule],
+    });
     service = TestBed.inject(DialogService);
   });
 
